1e4e076f77ea1bf25234d3ad47c4e19abcce8552
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-02 14:30:10 +0000

    Inital set up for the app done. Also added home, login and signup pages/routes and tested in the browser - all working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>SocialApe</h1>
-    </div>
-  );
+// Pages
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
