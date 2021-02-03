0771367e65c0475ae38ba551a4785feb4babe1aa
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-03 10:03:56 +0000

    Started working on fetching screams data in the home page from the API/backend using axios

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 
 class Home extends Component {
+  componentDidMount() {
+    axios.get("/screams").then((res) => {});
+  }
   render() {
     return (
-      <Grid container>
+      <Grid container spacing={2}>
         <Grid item sm={8} xs={12}>
-          <h4>Screams</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper
-            magna non pellentesque bibendum. Phasellus sagittis, sem id tempus
-            varius, nunc velit consequat turpis, lobortis placerat leo elit a
-            neque. Fusce a varius nulla. Sed placerat ac orci ut aliquet.
+            SCREAMS - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis semper magna non pellentesque bibendum. Phasellus sagittis, sem
+            id tempus varius, nunc velit consequat turpis, lobortis placerat leo
+            elit a neque. Fusce a varius nulla. Sed placerat ac orci ut aliquet.
             Vestibulum pellentesque nibh a orci finibus dapibus. Vivamus
             lobortis dolor id vehicula lacinia. Praesent commodo semper
             consectetur. Sed lacinia nisi a sem tristique, efficitur luctus orci
@@ -22,12 +25,11 @@ class Home extends Component {
           </p>
         </Grid>
         <Grid item sm={4} xs={12}>
-          <h4>Profile</h4>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper
-            magna non pellentesque bibendum. Phasellus sagittis, sem id tempus
-            varius, nunc velit consequat turpis, lobortis placerat leo elit a
-            neque. Fusce a varius nulla. Sed placerat ac orci ut aliquet.
+            PROFILE - Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Duis semper magna non pellentesque bibendum. Phasellus sagittis, sem
+            id tempus varius, nunc velit consequat turpis, lobortis placerat leo
+            elit a neque. Fusce a varius nulla. Sed placerat ac orci ut aliquet.
             Vestibulum pellentesque nibh a orci finibus dapibus.
           </p>
         </Grid>
