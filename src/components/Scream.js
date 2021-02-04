dbef2c934d2a3c8815977bc87d05aa553e60c687
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-04 11:51:28 +0000

    Removed console log for data and also worked on removing warnings from the console

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 // Material UI Card imports
