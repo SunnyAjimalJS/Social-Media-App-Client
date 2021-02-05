b9866c99970cb9145b0bcebd06a1e07d29cc1e59
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-05 12:45:07 +0000

    Added a material ui Grid to place the login form centrally, also added app icon and login header to the form - all working in the browser

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import AppIcon from "../images/icon.png";
 
-const styles = {};
+// Material UI imports
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  formGrid: {
+    textAlign: "center",
+  },
+};
 
 class Login extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div>
-        <h1>Login page</h1>
-      </div>
+      <Grid container className={classes.formGrid}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt="App Icon" />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Login);
