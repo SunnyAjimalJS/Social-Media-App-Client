c7b857c03fcfc8953314265288b7285474880745
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-11 16:55:11 +0000

    Signup form and Auth state complete - AuthRoutes also working and redirecting to home if a logged in user tries clicking on signup or login!

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import jwtDecode from "jwt-decode";
 // Components
 import Navbar from "./components/Navbar";
+import AuthRoute from "./util/AuthRoute";
 // Pages
 import Home from "./pages/home";
 import Login from "./pages/login";
@@ -31,11 +32,16 @@ const theme = createMuiTheme({
   },
 });
 
+let authenticated;
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
     window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
   }
 }
 
@@ -49,8 +55,18 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={Login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={Signup}
+                  authenticated={authenticated}
+                />
               </Switch>
             </div>
           </Router>
