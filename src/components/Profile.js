a3064b99631d2d5360ec9498be64ab06d8d30bce
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-18 15:27:31 +0000

    Worked on the profile section to show data from the user object in state

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,66 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+// Redux imports
 import { connect } from "react-redux";
-
 // Material UI Imports
 import Button from "@material-ui/core/Button";
-
+import Paper from "@material-ui/core/Paper";
+import MuiLink from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 // Icons imports
+import LocationOn from "@material-ui/icons/LocationOn";
+import LinkIcon from "@material-ui/icons/Link";
+import CalendarToday from "@material-ui/icons/CalendarToday";
 
-const styles = {};
+const styles = {
+  paper: {
+    padding: 20,
+  },
+  profile: {
+    "& .image-wrapper": {
+      textAlign: "center",
+      position: "relative",
+      "& button": {
+        position: "absolute",
+        top: "80%",
+        left: "70%",
+      },
+    },
+    "& .profile-image": {
+      width: 200,
+      height: 200,
+      objectFit: "cover",
+      maxWidth: "100%",
+      borderRadius: "50%",
+    },
+    "& .profile-details": {
+      textAlign: "center",
+      "& span, svg": {
+        verticalAlign: "middle",
+      },
+      "& a": {
+        color: "#00bcd4",
+      },
+    },
+    "& hr": {
+      border: "none",
+      margin: "0 0 10px 0",
+    },
+    "& svg.button": {
+      "&:hover": {
+        cursor: "pointer",
+      },
+    },
+  },
+  buttons: {
+    textAlign: "center",
+    "& a": {
+      margin: "20px 10px",
+    },
+  },
+};
 
 class Profile extends Component {
   render() {
@@ -19,7 +71,41 @@ class Profile extends Component {
       },
       loading,
     } = this.props;
-    return <div></div>;
+
+    let profileMarkup = !loading ? (
+      authenticated ? (
+        <Paper className={classes.paper}>
+          <div className={classes.profile}>
+            <div className="profile-image">
+              <img src={imageUrl} alt="profile" />
+            </div>
+            <hr />
+            <div className="profile-details">
+              <MuiLink
+                component={Link}
+                to={`/users/${handle}`}
+                color="secondary"
+                variant="h5"
+              >
+                @{handle}
+              </MuiLink>
+              <hr />
+              {bio && <Typography variant="body2">{bio}</Typography>}
+              <hr />
+              {location && (
+
+              )}
+            </div>
+          </div>
+        </Paper>
+      ) : (
+        <p>No profile - signup or login</p>
+      )
+    ) : (
+      <p>Loading...</p>
+    );
+
+    return profileMarkup;
   }
 }
 
