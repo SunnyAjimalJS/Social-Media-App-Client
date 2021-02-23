bb55ebe33cd1fcb355fcdda360ff42bff226c731
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-23 13:52:17 +0000

    Added a new action for editUserDetails which passes a userDetauls object and posts it the /user route and then dispatched the getUserData action to get the new updated userData

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -1,4 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import { connect } from "react-redux";
+
+const styles = (theme) => ({
+  ...theme,
+});
 
 class EditDetails extends Component {
   render() {
