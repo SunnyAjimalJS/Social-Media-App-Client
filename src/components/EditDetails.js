6fbf2e8b3e940b99cdc3505a004418e46bad755d
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-23 14:09:09 +0000

    Made EditDetails a connected component and passed it the editUserDetails action, also declared propTypes and set empty state for the profile fields which will be set to user credentials using mapStateToProps

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -3,15 +3,33 @@ import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 import { connect } from "react-redux";
+import { editUserDetails } from "../redux/actions/userActions";
 
 const styles = (theme) => ({
   ...theme,
 });
 
 class EditDetails extends Component {
+  state = {
+    bio: "",
+    location: "",
+    website: "",
+    open: false,
+  };
   render() {
     return <div></div>;
   }
 }
 
-export default EditDetails;
+EditDetails.propTypes = {
+  editUserDetails: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  credentials: state.user.credentials,
+});
+
+export default connect(mapStateToProps, { editUserDetails })(
+  withStyles(styles)(EditDetails)
+);
