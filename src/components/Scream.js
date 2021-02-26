a0226c478f929f74980cc7d5d5a1b12b1ab8e0b9
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-02-26 15:27:28 +0000

    Added a likeScream function which checks if a user has liked this scream by checking their like object. If there's a screamId that matches this scream's screamId then the function will return true which will change the icon to a filled heart rather than the empty hear icon

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -4,11 +4,14 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
 // Material UI imports
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+// Icons
+import ChatIcon from "@material-ui/icons/Chat";
 // Redux imports
 import { connect } from "react-redux";
 import { likeScream, unlikeScream } from "../redux/actions/dataActions";
@@ -28,6 +31,16 @@ const styles = {
 };
 
 class Scream extends Component {
+  likedScream = () => {
+    if (
+      this.props.user.likes &&
+      this.props.user.likes.find(
+        (like) => like.screamId === this.props.scream.screamId
+      )
+    )
+      return true;
+    else return false;
+  };
   render() {
     dayjs.extend(relativeTime);
     const {
@@ -63,6 +76,12 @@ class Scream extends Component {
             {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
+          {likeButton}
+          <span>{likeCount} Likes</span>
+          <MyButton tip="comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount}</span>
         </CardContent>
       </Card>
     );
