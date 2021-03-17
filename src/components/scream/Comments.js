a265369c4f29d2c06e07af790af48fcb4f2bf941
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-03-17 12:00:07 +0000

    Displaying of comments within the scream dialog complomplete - all working in the browser!

--- src/components/scream/Comments.js
+++ src/components/scream/Comments.js
@@ -12,6 +12,20 @@ const styles = {
     border: "none",
     margin: 4,
   },
+  visibleSeparator: {
+    width: "100%",
+    borderBottom: "1px solid rgba(0,0,0,0.1)",
+    marginBottom: 20,
+  },
+  commentImage: {
+    maxWidth: "100%",
+    height: 90,
+    objectFit: "cover",
+    borderRadius: "50%",
+  },
+  commentData: {
+    marginLeft: 20,
+  },
 };
 
 class Comments extends Component {
@@ -19,37 +33,43 @@ class Comments extends Component {
     const { classes, comments } = this.props;
     return (
       <Grid container>
-        {comments.map((comment) => {
+        {comments.map((comment, index) => {
           const { body, createdAt, userImage, userHandle } = comment;
-          <Fragment key={createdAt}>
-            <Grid item sm={12}>
-              <Grid container>
-                <Grid item sm={2}>
-                  <img
-                    src={userImage}
-                    alt="comment"
-                    className={classes.commmentImage}
-                  />
-                </Grid>
-                <Grid item sm={9}>
-                  <div className={classes.commentData}>
-                    <Typography
-                      variant="h5"
-                      component={Link}
-                      to={`/users/${userHandle}`}
-                      color="primary"
-                    >
-                      {userHandle}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
-                    </Typography>
-                    <hr className={classes.invisibleSeparator} />
-                  </div>
+          return (
+            <Fragment key={createdAt}>
+              <Grid item sm={12}>
+                <Grid container>
+                  <Grid item sm={2}>
+                    <img
+                      src={userImage}
+                      alt="comment"
+                      className={classes.commentImage}
+                    />
+                  </Grid>
+                  <Grid item sm={9}>
+                    <div className={classes.commentData}>
+                      <Typography
+                        variant="h5"
+                        component={Link}
+                        to={`/users/${userHandle}`}
+                        color="primary"
+                      >
+                        {userHandle}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        {dayjs(createdAt).format("h:mm a, MMMM DD YYYY")}
+                      </Typography>
+                      <hr className={classes.invisibleSeparator} />
+                      <Typography variant="body1">{body}</Typography>
+                    </div>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </Fragment>;
+              {index !== comments.length - 1 && (
+                <hr className={classes.visibleSeparator} />
+              )}
+            </Fragment>
+          );
         })}
       </Grid>
     );
