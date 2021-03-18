b48ce1edb2e80a26eac3866a372dbe5bb1ed062d
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-03-18 10:42:48 +0000

    Started building the user component

---
+++ src/pages/user.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Scream from "../components/scream/Scream";
+import Grid from "@material-ui/core/Grid";
+
+// Redux imports
+import { connect } from "react-redux";
+import { getUserData } from "../redux/actions/dataActions";
+
+export class user extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default user;
