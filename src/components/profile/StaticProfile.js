c7adcf1cb898c5562623ad22338295e613e4422e
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-03-18 12:08:00 +0000

    User page component complete, started working on a Static Profile component to display user information

---
+++ src/components/profile/StaticProfile.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import dayjs from "dayjs";
+// Material UI imports
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+// Icons
