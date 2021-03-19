8145d3601ee84c10081afd44a461fca8e8b9603b
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-03-19 11:05:07 +0000

    Created Notifications component, declared propTypes, set the notifications object inside mapStateToProps from the user's state and connected the component on export to handle the action of markNotificationsReadwhich will be created next!

--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -3,3 +3,36 @@ import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import PropTypes from "prop-types";
+// Material UI imports
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Menu";
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
+// Icons
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ChatIcon from "@material-ui/icons/Chat";
+// Redux imports
+import { connect } from "react-redux";
+import { markNotificationsRead } from "../../redux/actions/userActions";
+
+class Notifications extends Component() {
+  state = {
+    anchorEl: null,
+  };
+}
+
+Notifications.propTypes = {
+  markNotificationsRead: PropTypes.func.isRequired,
+  notifications: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  notifications: state.user.notifications,
+});
+
+export default connect(mapStateToProps, { markNotificationsRead })(
+  Notifications
+);
